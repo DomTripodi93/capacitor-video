6a9ece71c13f83916016028b9db4ef18365abcd1
Dominic Tripodi <dtripodi93@gmail.com>
2024-08-21 01:07:23 -0400

    Initial Commit

--- android/src/main/java/com/domtripcode/plugins/video/VideoRecorder.java
+++ android/src/main/java/com/domtripcode/plugins/video/VideoRecorder.java
@@ -1,11 +1,39 @@
-package com.domtripcode.plugins.video;
+import android.app.Activity;
+import android.content.Intent;
+import android.provider.MediaStore;
+import android.net.Uri;
+import com.getcapacitor.annotation.CapacitorPlugin;
+import com.getcapacitor.Plugin;
+import com.getcapacitor.PluginCall;
+import com.getcapacitor.PluginMethod;
+import com.getcapacitor.PluginResult;
 
-import android.util.Log;
+@CapacitorPlugin(name = "VideoRecorder")
+public class VideoRecorderPlugin extends Plugin {
 
-public class VideoRecorder {
+    static final int REQUEST_VIDEO_CAPTURE = 1;
+    private PluginCall call;
 
-    public String echo(String value) {
-        Log.i("Echo", value);
-        return value;
+    @PluginMethod
+    public void recordVideo(PluginCall call) {
+        this.call = call;
+        Intent takeVideoIntent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+        if (takeVideoIntent.resolveActivity(getContext().getPackageManager()) != null) {
+            startActivityForResult(call, takeVideoIntent, REQUEST_VIDEO_CAPTURE);
+        } else {
+            call.reject("Unable to open camera");
+        }
+    }
+
+    @Override
+    protected void handleOnActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_VIDEO_CAPTURE && resultCode == Activity.RESULT_OK) {
+            Uri videoUri = data.getData();
+            if (videoUri != null) {
+                call.resolve(new JSObject().put("videoUri", videoUri.toString()));
+            } else {
+                call.reject("Video URI is null");
+            }
+        }
     }
 }
--- ios/Sources/VideoRecorderPlugin/VideoRecorder.swift
+++ ios/Sources/VideoRecorderPlugin/VideoRecorder.swift
@@ -1,8 +1,39 @@
 import Foundation
+import Capacitor
+import AVFoundation
+import MobileCoreServices
 
-@objc public class VideoRecorder: NSObject {
-    @objc public func echo(_ value: String) -> String {
-        print(value)
-        return value
+@objc(VideoRecorderPlugin)
+public class VideoRecorderPlugin: CAPPlugin, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    var call: CAPPluginCall?
+
+    @objc func recordVideo(_ call: CAPPluginCall) {
+        self.call = call
+        DispatchQueue.main.async {
+            if UIImagePickerController.isSourceTypeAvailable(.camera) {
+                let videoPicker = UIImagePickerController()
+                videoPicker.delegate = self
+                videoPicker.sourceType = .camera
+                videoPicker.mediaTypes = [kUTTypeMovie as String]
+                self.bridge?.viewController?.present(videoPicker, animated: true, completion: nil)
+            } else {
+                call.reject("Camera not available")
+            }
+        }
+    }
+
+    public func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        picker.dismiss(animated: true, completion: nil)
+        if let videoURL = info[UIImagePickerController.InfoKey.mediaURL] as? URL {
+            call?.resolve(["videoUrl": videoURL.absoluteString])
+        } else {
+            call?.reject("Failed to capture video")
+        }
+    }
+
+    public func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+        call?.reject("User cancelled video recording")
     }
 }
